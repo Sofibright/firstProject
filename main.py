1004c19427a6a27d1c8f804877e4042a6a69c8fd
Sofibright <coaz@mail.ru>
2022-04-10 00:17:34 +1000

    to see no - a,b

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def no(a, b):
+    print('do nothing', a, b)
+    return 'not none'
+
 print('hekko')
 a = 1; b = 2; print(a, b)
 a = 5
@@ -10,3 +14,5 @@ elif a == 0:
     print('false')
 else:
     print('true')
+buf = no('s', 5)
+print(buf)
