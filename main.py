7ed826d0920f22ef78534c0e58f570a8479235ba
Sofibright <coaz@mail.ru>
2022-04-09 23:54:18 +1000

    replace script hekko if-else

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('hekko')
+a = 1; b = 2; print(a, b)
+a = 5
+if a == 5:
+    print('Ура!')
+a = int(input())
+if a < 0:
+    print('false')
+elif a == 0:
+    print('false')
+else:
+    print('true')
